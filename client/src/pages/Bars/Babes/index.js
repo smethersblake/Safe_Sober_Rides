8da0f580b367c9be0e96d3a0d2b4287c9ff613f3
Blake Smethers <96027095+smethersblake@users.noreply.github.com>
2022-11-23 16:17:46 -0600

    Update index.js

--- client/src/pages/Bars/Babes/index.js
+++ client/src/pages/Bars/Babes/index.js
@@ -9,8 +9,14 @@ function Babes ()
         console.log('click')
     }
     return (
-        <div>
-            <h1 className="BarName">Babes</h1>
+        <div className="container mx-auto">
+            <div className="columns-2">
+                <h1 className="text-3xl font-bold flex justify-end">Babes Music Bar</h1>
+                <p className="flex justify-end text-sm">20685 Holyoke Avenue S. Lakeville, MN 55044</p>
+            </div>
+            <div>
+                
+            </div>
             <Review />
             <a id="webLink" onClick={handleClick} href="https://www.babesmusicbar.com/" rel="noreferrer" target="_blank" >website</a>
             <ul>
